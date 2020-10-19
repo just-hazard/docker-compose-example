2a72f7d9f2d259a8c03991f07801fdbf52b9fbd3
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-10-19 14:44:53 +0900

    docker-compose sample

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,23 +1,16 @@
 package hello
 import io.javalin.Javalin
 import khttp.get
-import java.io.FileInputStream
 import java.util.*
 
 fun main(args : Array<String>) {
-    val props = Properties()
-    props.load(FileInputStream("mockserverdemo.properties"))
-
-    startWebServer(props)
+    startWebServer()
 }
 
-private fun startWebServer(props: Properties) {
+private fun startWebServer() {
     val app = Javalin.create().start(8080)
-    app.get("/test") { ctx -> ctx.result(getCurrentUnixTime(props.getProperty("test.timeApiUrl")).toString()) }
-    app.get("/prod") { ctx -> ctx.result(getCurrentUnixTime(props.getProperty("prod.timeApiUrl")).toString()) }
+    app.get("/test") { ctx -> ctx.result(get("http://host.docker.internal:1080/test").text) }
 }
 
-private fun getCurrentUnixTime(queryUri: String): Long {
-    return get(queryUri).jsonObject.getLong("unixtime")
-}
+
 
